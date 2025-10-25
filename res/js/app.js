1b0a8031e59c054c43ccf0c8495ea24feb251d85
Honzak <jfedor06@gmail.com>
2025-10-26 00:23:22 +0200

    hamburger menu done

--- res/js/app.js
+++ res/js/app.js
@@ -1,13 +1,46 @@
 const menuBtn = document.getElementById('menuBtn')
+const menuBtn2 = document.getElementById('menuBtn2')
 const aboutBtn = document.getElementById('aboutBtn')
 const contactBtn = document.getElementById('contactBtn')
+const mobileMenu = document.getElementById('mobileMenu')
+const clickSpace = document.getElementById('clickSpace')
+const hamburgerIcon = document.getElementById('hamburgerIcon')
 
-univOnclick = (btn, loc) => {
-    btn.onclick = () => {
-        location.href = loc
+let count = 0
+
+univLocation = (btn, loc) => {
+    if (btn) {
+        btn.onclick = () => {
+            location.href = loc
+        }
     }
 }
 
-univOnclick(menuBtn, 'menu.html')
-univOnclick(aboutBtn, 'o_nas.html')
-univOnclick(contactBtn, 'kontakt.html')
+univLocation(menuBtn, 'menu.html')
+univLocation(menuBtn2, 'menu.html')
+univLocation(aboutBtn, 'o_nas.html')
+univLocation(contactBtn, 'kontakt.html')
+
+clickSpace.onclick = () => {
+    mobileMenu.style.display = 'none'
+    hamburgerIcon.src = './res/svg/PhList.svg'
+    count = 0
+}
+
+hamburgerIcon.onclick = () => {
+    console.log(count)
+    if (count == 0) {
+        mobileMenu.style.display = 'block'
+        hamburgerIcon.src = './res/svg/PhX.svg'
+        count++
+    }
+    else if (count == 1) {
+        mobileMenu.style.display = 'none'
+        hamburgerIcon.src = './res/svg/PhList.svg'
+        count--
+    }
+}
+
+if (document.body.style.width >= 768) {
+    mobileMenu.style.display = 'none'
+}
